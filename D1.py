b68a78b5d23527aacb26e365285c1216478c975c
RealSense3D <ipgedeyouxiang@163.com>
2018-03-22 19:42:38 +0800

    about: string

--- D1.py
+++ D1.py
@@ -83,10 +83,37 @@
 #     i += 1
 # print('sum =', a)
 
-a = '[08:39:26.220950][LIB][esti_key]nf_WAVE_MW_Send_Wsm_Unsecured(343)SEND_WAVE_START'
-print(a[0:1])
-print(a.find('.'))
-print(a.find(']'))
-print(a[a.find(']')])
-print(a[(a.find('.')+1):(a.find(']'))])
+a = '[06:09:32.197998][LIB][esti_wave]nf_WAVE_MW_Send_Wsm_Unsecured(343)MW send start'
+b = '[06:09:32.199068][LIB][esti_wave]nf_wave_wsmp_wave_short_message_reuest(62)wsm encode start'
+c = '[06:09:32.200052][LIB][esti_wave]nf_wave_wsmp_wave_short_message_reuest(225)wsm encode end'
+d = '[06:09:32.201006][LIB][esti_wave]nf_wave_wsmp_wave_short_message_reuest(244)wsm send mailbox'
+e = '[06:09:32.202000][LIB][esti_wave]nff_wave_wsmp_send_task(37)wsm receive mailbox'
+f = '[06:09:32.204960][LIB][esti_wave]nff_wave_wsmp_send_task(40)socket send start'
+g = '[06:09:32.207050][LIB][esti_wave]nff_wave_wsmp_socket_send(170)socket send end'
+
 print(a.count(']'))
+
+a_i = int(a[(a.find('.')+1):(a.find(']'))])
+b_i = int(b[(b.find('.')+1):(b.find(']'))])
+c_i = int(c[(c.find('.')+1):(c.find(']'))])
+d_i = int(d[(d.find('.')+1):(d.find(']'))])
+e_i = int(e[(e.find('.')+1):(e.find(']'))])
+f_i = int(f[(f.find('.')+1):(f.find(']'))])
+g_i = int(g[(g.find('.')+1):(g.find(']'))])
+
+
+
+print('a_i =', a_i)
+print('type(a_i) =', type(a_i))
+print('b_i =', b_i)
+print('type(b_i) =', type(b_i))
+print('c_i =', c_i)
+print('type(c_i) =', type(c_i))
+print('d_i =', d_i)
+print('type(d_i) =', type(d_i))
+print('e_i =', e_i)
+print('type(e_i) =', type(e_i))
+print('f_i =', f_i)
+print('type(f_i) =', type(f_i))
+print('g_i =', g_i)
+print('type(g_i) =', type(g_i))
